8c5c307c0e8aa812bbbe6defadea85b9582dbada
Phat Nguyen <masterrio2412@gmail.com>
2025-07-31 13:55:10 +0700

    fix css

--- ShopNongSan/vite.config.js
+++ ShopNongSan/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+  // Hook sau khi build để copy static.json vào dist
+  closeBundle: () => {
+    const srcPath = path.resolve(__dirname, "static.json");
+    const destPath = path.resolve(__dirname, "dist", "static.json");
+    fs.copyFileSync(srcPath, destPath);
+  },
+});
