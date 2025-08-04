1b29a1f2a9b714d008cabcc327505dab4b9e6c67
Phat Nguyen <masterrio2412@gmail.com>
2025-08-04 22:34:09 +0700

    rencent product

--- ShopNongSan/src/components/Home/Main/home.recentlyView.jsx
+++ ShopNongSan/src/components/Home/Main/home.recentlyView.jsx
@@ -31,35 +31,50 @@ const RecentlyViewedProducts = () => {
 
   return (
     <div className="recently-viewed-products">
-      <h2>Đã xem gần đây </h2>
-      <div className="product-list">
-        {recentlyViewedProducts.map((product) => (
-          <div key={product.id} className="product-item">
-            <div className="product-image">
-              <img
-                src={`${import.meta.env.VITE_DATABASE_URL}${product.img}`}
-                alt={product.name}
-                onClick={() => handleShowDetails(product)}
-              />
-            </div>
-            <div className="product-details">
-              <div className="product-name">{product.name}</div>
-              <span className="product-price">${product.price.toFixed(2)}</span>
-              {product.price_old && (
-                <span className="product-price-old">${product.price_old}</span>
-              )}
-              <div>
-                {Array.from({ length: product.rating }).map((_, index) => (
-                  <span key={index}>⭐</span>
-                ))}
+      {recentlyViewedProducts.length > 0 ? (
+        <>
+          <h3>Đã xem gần đây </h3>
+          <div className="product-list">
+            {recentlyViewedProducts.map((product) => (
+              <div key={product.id} className="product-item">
+                <div className="product-image">
+                  <img
+                    src={`${import.meta.env.VITE_DATABASE_URL}${product.img}`}
+                    alt={product.name}
+                    onClick={() => handleShowDetails(product)}
+                  />
+                </div>
+                <div className="product-details">
+                  <div className="product-name">{product.name}</div>
+                  <span className="product-price">
+                    ${product.price.toFixed(2)}
+                  </span>
+                  {product.price_old && (
+                    <span className="product-price-old">
+                      ${product.price_old}
+                    </span>
+                  )}
+                  <div>
+                    {Array.from({ length: product.rating }).map((_, index) => (
+                      <span key={index}>⭐</span>
+                    ))}
+                  </div>
+                </div>
+                {product.discount && (
+                  <span className="product-discount">
+                    Sale {product.discount}%
+                  </span>
+                )}
               </div>
-            </div>
-            {product.discount && (
-              <span className="product-discount">Sale {product.discount}%</span>
-            )}
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      ) : (
+        <div className="no-recently-viewed">
+          Chưa có sản phẩm nào được xem gần đây
+        </div>
+      )}
+
       <ProductDetails
         ProductDetails={selectedProduct}
         show={showDetails}
