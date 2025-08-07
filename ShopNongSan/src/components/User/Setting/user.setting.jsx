58601eabb3c61878b67479f7b61019aacaf15b08
Phat Nguyen <masterrio2412@gmail.com>
2025-08-07 22:56:54 +0700

    icon margin

--- ShopNongSan/src/components/User/Setting/user.setting.jsx
+++ ShopNongSan/src/components/User/Setting/user.setting.jsx
@@ -13,7 +13,7 @@ const UserSetting = () => {
       </Container>
       <div>
         <img
-          src="../../../public/Image/Login/Breadcrumbs (1).png"
+          src={`${import.meta.env.VITE_DATABASE_URL}/public/Image/Login/Breadcrumbs (1).png`}
           alt="Logo"
         ></img>
       </div>
