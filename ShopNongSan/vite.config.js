7ea6e5fb50469872d9038c46d787992b48bc9725
Phat Nguyen <masterrio2412@gmail.com>
2025-08-18 22:43:10 +0700

    fix navigation

--- ShopNongSan/vite.config.js
+++ ShopNongSan/vite.config.js
@@ -1,29 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import fs from "fs";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "copy-static-json",
-      closeBundle() {
-        const srcPath = path.resolve(__dirname, "static.json");
-        const destPath = path.resolve(__dirname, "dist", "static.json");
-        if (fs.existsSync(srcPath)) {
-          fs.copyFileSync(srcPath, destPath);
-          console.log("✅ Đã copy static.json vào dist/");
-        } else {
-          console.warn("⚠️ Không tìm thấy static.json");
-        }
-      },
-    },
-  ],
+  plugins: [react()],
   build: {
     outDir: "dist",
     chunkSizeWarningLimit: 1500,
